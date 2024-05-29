a4fcf5a2bf4e47d3a615d158584e686793ea73dd
markgregg <gregg.mark@gmail.com>
2024-05-29 18:54:28 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "enhanced-input-examples",
+  base: "react-smart-search-examples",
   plugins: [
     react(),
     eslintPlugin(),
